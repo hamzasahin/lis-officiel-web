f2fa9ef2e059b46ed0191742f85ef0058e414506
GitBuster <hamzashnnn@gmail.com>
2025-01-15 00:30:21 -0800

    Update Heroku postbuild script and improve catchall route handling
    - Simplified the Heroku postbuild script in package.json to only run the build command.
    - Enhanced the catchall route in server.js to provide clearer documentation for handling unmatched requests by serving React's index.html file.

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const app = express();
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'build')));
 
-// Handle any requests that don't match the above
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
